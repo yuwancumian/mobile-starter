cb807c20c0ca17df34ac4d007050cfa3f0829f99
yuwancumian <easier2005@163.com>
2016-09-23 14:57:25 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+a starter for wxsite
+
+bootstrap 3.2
+jquery 2.0.1
---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+
+var connect = require('gulp-connect');
+var watch = require('gulp-watch');
+var less = require('gulp-less');
+var jade = require('gulp-jade');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+//var livereload = require('gulp-livereload');
+ 
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true,
+		root: 'html'
+		//host: 'ajia.dev'
+  });
+});
+
+/*
+gulp.task('livereload', function() {
+  gulp.src(['html/*.html', 'html/css/*.css'])
+		.pipe(watch(['html/*.html','html/css/*.css','html/js/*.js']))
+    .pipe(connect.reload());
+});
+*/
+gulp.task('html',function(){
+  	gulp.src('app/*.html')
+    .pipe(gulp.dest('html/'))
+}); 
+
+gulp.task('less',function(){
+  gulp.src('app/less/main.less')
+    .pipe(less())
+		// .pipe(concat('all.css'))
+		.pipe(gulp.dest('html/css/'))
+		.pipe(minifyCss())
+		.pipe(rename('main.min.css'))
+		.pipe(gulp.dest('html/css'))
+});
+
+gulp.task('js',function(){
+	gulp.src('app/js/*.js')
+	.pipe(gulp.dest('html/js'))
+});
+
+gulp.task('img',function(){
+	gulp.src('app/images/*.*')
+	.pipe(gulp.dest('html/images'))
+});
+ 
+
+gulp.task('watch', function() {
+  gulp.watch('app/*.html', ['html']);
+  gulp.watch('app/less/*.less', ['less']);
+  gulp.watch('app/js/*.js', ['js']);
+	gulp.watch('app/images/', ['img']);
+})
+
+
+gulp.task('style',['less','css']);
+gulp.task('default', ['html', 'less', 'js', 'img', 'webserver', 'watch']);
